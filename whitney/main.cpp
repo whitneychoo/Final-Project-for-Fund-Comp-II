5fc9da5c64666646661fc03680dd5744b9851341
jyoon2 <jyoon2@nd.edu>
2015-04-23 21:08:59 -0400

    version with two derived classes

---
+++ whitney/main.cpp
@@ -0,0 +1,25 @@
+#include "base.h"
+#include "joseph.h"
+#include "Whitney.h"
+#include <iostream>
+using namespace std;
+
+int main( int argc, char* args[] ){
+
+	Joseph hi;
+	Whitney hoe;
+	//Start up SDL and create window
+	if( !hi.init())
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{	
+	hi.playGame();
+	cout << "Whitney is playing" << endl;
+        hoe.playGame();
+	cout << "Whitney is done" << endl;
+	}
+	hi.close();
+	hoe.close();
+}
